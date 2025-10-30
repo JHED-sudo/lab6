7173b5dd179f041a537d63c23e99a9269c40525d
Jhed B. Gabunada <jhedgabunada@gmail.com>
2025-10-30 18:30:47 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // The overall app title (optional)
+      title: 'Anonymous Group App',
+      home: Scaffold(
+        // The background color is set here!
+        backgroundColor: Colors.blueGrey[50],
+        body: Center(
+          child: Column(
+            // To prevent the column from taking up the full screen height
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text(
+                'ANONYMOUS GROUP',
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                ),
+              ),
+              const Text(
+                'Jhed B. Gabunada',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.pink
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                '3A-AI',
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black54
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
